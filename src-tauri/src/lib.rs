c398f8f0dc14ccf23a2abc20ed62a46f9d52a750
VivinMeth <vivinmeth@surkyl.com>
2025-02-06 06:04:43 +0530

    #main [run] made dashboard open in a new window

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,10 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 //
 
-use std::any::Any;
+use std::str::FromStr;
 
-use tauri::{ipc::IpcResponse, menu::*, Manager};
+use rand;
+use tauri::{ipc::IpcResponse, menu::*, Manager, Url, WebviewUrl, WebviewWindowBuilder};
 
 #[tauri::command]
 fn _greet(name: &str) -> String {
@@ -30,18 +31,35 @@ pub fn run() {
 
             app.on_menu_event(move |app, ev| {
                 let main_window = app.get_webview_window("main").unwrap();
+                let random_number = rand::random::<i64>();
                 if *ev.id() == home_menu_item_id {
                     main_window.eval(&format!(
                         "window.location.replace('http://localhost:{}')",
                         "3000"
                     ));
                 } else if *ev.id() == dashboard_menu_item_id {
-                    main_window.eval(&format!(
-                        "window.location.replace('http://localhost:{}')",
-                        "7878"
-                    ));
+                    // main_window.eval(&format!(
+                    //     "window.location.replace('http://localhost:{}')",
+                    //     "7878"
+                    // ));
+
+                    let window = WebviewWindowBuilder::new(
+                        app,
+                        random_number.to_string(),
+                        tauri::WebviewUrl::External(
+                            Url::from_str("http://localhost:7878/panel").unwrap(),
+                        ),
+                    )
+                    .title("OpenWebUI Dashboard")
+                    .focused(true)
+                    .min_inner_size(1366.0, 768.0)
+                    .visible(true)
+                    .build()
+                    .unwrap();
+                    window.show().unwrap();
                 }
             });
+
             Ok(())
         })
         .run(tauri::generate_context!())
