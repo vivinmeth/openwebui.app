2fc53319b177ddd856b915e12a3a5ea82c3d0723
VivinMeth <vivinmeth@surkyl.com>
2025-02-06 06:06:03 +0530

    #main [run] made chat open in a new window

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -18,7 +18,7 @@ pub fn run() {
             let menu = app.menu().expect("menu not found!");
             // let go_submenu = menu.get("Go").expect("go menu not found!");
             // println!("{:?}", go_submenu.id());
-            let home_menu_item = MenuItem::new(handle, "Home", true, None::<&str>)?;
+            let home_menu_item = MenuItem::new(handle, "Chat", true, None::<&str>)?;
             let home_menu_item_id = home_menu_item.id().clone();
             let dashboard_menu_item = MenuItem::new(handle, "Dashboard", true, None::<&str>)?;
             let dashboard_menu_item_id = dashboard_menu_item.id().clone();
@@ -30,13 +30,23 @@ pub fn run() {
             let _ = app.set_menu(menu);
 
             app.on_menu_event(move |app, ev| {
-                let main_window = app.get_webview_window("main").unwrap();
+                let _main_window = app.get_webview_window("main").unwrap();
                 let random_number = rand::random::<i64>();
                 if *ev.id() == home_menu_item_id {
-                    main_window.eval(&format!(
-                        "window.location.replace('http://localhost:{}')",
-                        "3000"
-                    ));
+                    let window = WebviewWindowBuilder::new(
+                        app,
+                        random_number.to_string(),
+                        tauri::WebviewUrl::External(
+                            Url::from_str("http://localhost:3000").unwrap(),
+                        ),
+                    )
+                    .title("OpenWebUI Dashboard")
+                    .focused(true)
+                    .min_inner_size(1366.0, 768.0)
+                    .visible(true)
+                    .build()
+                    .unwrap();
+                    window.show().unwrap();
                 } else if *ev.id() == dashboard_menu_item_id {
                     // main_window.eval(&format!(
                     //     "window.location.replace('http://localhost:{}')",
