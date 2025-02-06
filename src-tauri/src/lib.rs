c2dfa177fb72b4a2d178cc36d91124df740a8c38
VivinMeth <vivinmeth@surkyl.com>
2025-02-06 05:43:33 +0530

    #main [run] added Nav menu for navigating between chat and dashboard pages

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,49 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+//
+
+use std::any::Any;
+
+use tauri::{ipc::IpcResponse, menu::*, Manager};
+
 #[tauri::command]
-fn greet(name: &str) -> String {
+fn _greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let handle = app.handle();
+            let menu = app.menu().expect("menu not found!");
+            // let go_submenu = menu.get("Go").expect("go menu not found!");
+            // println!("{:?}", go_submenu.id());
+            let home_menu_item = MenuItem::new(handle, "Home", true, None::<&str>)?;
+            let home_menu_item_id = home_menu_item.id().clone();
+            let dashboard_menu_item = MenuItem::new(handle, "Dashboard", true, None::<&str>)?;
+            let dashboard_menu_item_id = dashboard_menu_item.id().clone();
+            let sub_menu = SubmenuBuilder::new(handle, "Nav")
+                .items(&[&home_menu_item, &dashboard_menu_item])
+                .build()?;
+
+            menu.append(&sub_menu)?;
+            let _ = app.set_menu(menu);
+
+            app.on_menu_event(move |app, ev| {
+                let main_window = app.get_webview_window("main").unwrap();
+                if *ev.id() == home_menu_item_id {
+                    main_window.eval(&format!(
+                        "window.location.replace('http://localhost:{}')",
+                        "3000"
+                    ));
+                } else if *ev.id() == dashboard_menu_item_id {
+                    main_window.eval(&format!(
+                        "window.location.replace('http://localhost:{}')",
+                        "7878"
+                    ));
+                }
+            });
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
